af27367e617586f7b648bb5e2dac646263c0eb45
Aryan <aryanbisht9458@gmail.com>
2022-03-14 09:00:18 +0530

    Now can download and send videos also.

--- app/src/main/java/com/example/whatsappdownloadertry/showImageActivity.java
+++ app/src/main/java/com/example/whatsappdownloadertry/showImageActivity.java
@@ -1,15 +1,23 @@
 package com.example.whatsappdownloadertry;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ShareCompat;
 import androidx.core.content.FileProvider;
 
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.FileUtils;
+import android.os.ParcelFileDescriptor;
 import android.os.StrictMode;
 import android.provider.MediaStore;
 import android.util.Log;
@@ -19,7 +27,9 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.example.whatsappdownloadertry.databinding.ActivityShowImageBinding;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileDescriptor;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -65,24 +75,72 @@ public class showImageActivity extends AppCompatActivity {
         binding.shareButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(showImageActivity.this, "Downloading is started", Toast.LENGTH_SHORT).show();
+
                 StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
                 StrictMode.setVmPolicy(builder.build());
-                Uri uri = startCopyFiles(finalSource);
+                Uri uri = finalSource;
 
-                Intent intent = new Intent(android.content.Intent.ACTION_SEND);
-                intent.setType("image/*");
-                intent.putExtra(Intent.EXTRA_STREAM,uri);
-                startActivity(Intent.createChooser(intent, "Share App Link Via :"));
+                try {
+                    if(uri.toString().endsWith(".jpg"))
+                        shareImage(uri.getPath());
+                    else
+                        shareVideo(uri.getPath());
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
 
-    public static Uri startCopyFiles(Uri finalSource)
+    public void shareImage(String filepath) throws FileNotFoundException {
+        Intent shareIntent = new Intent(android.content.Intent.ACTION_SEND);
+        shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        shareIntent.setType("image/*");
+
+        File filesDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), filepath);
+        String mediaPath = MediaStore.Images.Media.insertImage(getContentResolver(), filepath, filesDir.getName(), null);
+        Uri uri = Uri.parse(mediaPath);
+        shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        shareIntent.putExtra(Intent.EXTRA_STREAM, uri);
+
+        try {
+            startActivity(shareIntent);
+        }
+        catch (android.content.ActivityNotFoundException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    public void shareVideo(String filepath){
+        Intent shareintent=new Intent("android.intent.action.SEND");
+        shareintent.setType("video/mp4");
+        shareintent.putExtra("android.intent.extra.STREAM", Uri.parse(filepath));
+        startActivity(Intent.createChooser(shareintent,"share"));
+
+    }
+
+    public static Uri startCopyFiles(@NonNull Uri finalSource)
     {
-        String lastName = Calendar.getInstance().getTimeInMillis()+".jpg";
-        String destination = Environment.getExternalStorageDirectory().getAbsolutePath() + "/DCIM/Camera/" + lastName;
-        copyFile(finalSource.getPath(),destination);
+        String destination = Environment.getExternalStorageDirectory().getAbsolutePath() + "/DCIM/Camera/";
+        File file = new File(destination);
+
+        String lastName = (new File(finalSource.getPath())).getName();
+
+        if(!file.exists())
+            file.mkdir();
+
+        destination = destination + lastName;
+        File[] allfiles = file.listFiles();
+        boolean found=true;
+        for(File data:allfiles)
+        {
+            if(data.getName().equals(lastName)){
+                found = false;
+                break;
+            }
+        }
+        if(found)
+            copyFile(finalSource.getPath(),destination);
 
         return Uri.fromFile(new File(destination));
     }
